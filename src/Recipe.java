c5698ebb5aa8147d5435a6fce5ed5669dcf90e0b
yoshidki <kiyotaka.yoshida@justsystems.com>
2015-12-22 14:47:16 +0900

    spec5:レシピIDを出力するようにしました。

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,8 +1,8 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.nio.charset.Charset;
 
 /**
@@ -29,10 +29,10 @@ public class Recipe {
 	 *            レシピファイル名
 	 */
 	public void readFile(String name) {
-		try (BufferedReader reader = new BufferedReader(
+		try (LineNumberReader reader = new LineNumberReader(
 				new InputStreamReader(new FileInputStream(new File(name)), Charset.forName(CHARSET)));) {
 			for (String line; (line = reader.readLine()) != null;) {
-				System.out.println(line);
+				System.out.println(reader.getLineNumber() + ":" + line);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
